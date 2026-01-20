e483cbfe5c6525c57bca354f5ba67315c46ff7a6
Devendra Negi <devnegi@Devendras-MacBook-Air.local>
2026-01-20 21:30:08 +0530

    start

---
+++ apps/inventory/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="inventory" />
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/platform/src/app/app.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import NxWelcome from './nx-welcome';
+import { Link, Route, Routes } from 'react-router-dom';
+
+const Inventory = React.lazy(() => import('inventory/Module'));
+
+export function App() {
+  return (
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/inventory">Inventory</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="platform" />} />
+        <Route path="/inventory" element={<Inventory />} />
+      </Routes>
+    </React.Suspense>
+  );
+}
+
+export default App;
