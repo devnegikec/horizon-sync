d1f7a142947c3bcd808b73adb2b33f8b76607483
Devendra Negi <devnegikec@gmail.com>
2026-04-29 18:32:43 +0530

    updated env

--- apps/platform/src/environments/environment.ts
+++ apps/platform/src/environments/environment.ts
@@ -3,7 +3,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: process.env.NX_API_BASE_URL || 'https://f3ff-2409-40c2-11ae-5835-b963-ef28-78f4-c277.ngrok-free.app',
-  apiCoreUrl: process.env.NX_API_CORE_URL || 'https://f3ff-2409-40c2-11ae-5835-b963-ef28-78f4-c277.ngrok-free.app',
-  searchApiBaseUrl: process.env.NX_SEARCH_API_BASE_URL || 'https://f3ff-2409-40c2-11ae-5835-b963-ef28-78f4-c277.ngrok-free.app',
+  apiBaseUrl: process.env.NX_API_BASE_URL || '',
+  apiCoreUrl: process.env.NX_API_CORE_URL || '',
+  searchApiBaseUrl: process.env.NX_SEARCH_API_BASE_URL || '',
 };
