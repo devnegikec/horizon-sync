7ca4f375c1ad7fa679f9533ebb83e74803900930
nyatender <48202861+nyatender@users.noreply.github.com>
2026-08-07 01:25:14 +0530

    wms/workes_loading_issue (#186)
    * wms/workes_loading_issue
    
    * Untrack .env
    
    ---------
    
    Co-authored-by: yaten1983 <negi.yaten@gmail.com>

--- apps/inventory/src/environments/environment.ts
+++ apps/inventory/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiCoreUrl: process.env.NX_API_CORE_URL || 'http://localhost:8001',
   apiBaseUrl: process.env.NX_API_BASE_URL || 'http://localhost:8000',
-  apiIdentityUrl: process.env.NX_API_IDENTITY_URL || 'http://localhost:8000',
+  apiIdentityUrl: process.env.NX_API_IDENTITY_URL || process.env.NX_API_BASE_URL || 'http://localhost:8000',
 };
